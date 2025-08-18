8231b20733e149030ea7b68252e31070606d1065
HxWOO <kimjd766@gmail.com>
2025-08-18 18:41:43 +0900

    predict를 위해 Features 가져오는 함수 추가

--- mlops_inference_server/db.py
+++ mlops_inference_server/db.py
@@ -13,7 +13,7 @@ load_dotenv()
 logger = setup_logger(name="db")
 
 # 이제 os.environ을 통해 .env 파일에 정의된 변수를 사용할 수 있습니다.
-logger.log(msg="환경변수 설정")
+logger.info(msg="환경변수 설정")
 DB_HOST = os.environ.get('DB_HOST')
 DB_PORT = int(os.environ.get('DB_PORT'))
 DB_NAME = os.environ.get('DB_NAME')
@@ -21,7 +21,15 @@ DB_USER = os.environ.get('DB_USER')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 
 REGISTERED_MODEL_NAME = os.getenv("REGISTERED_MODEL_NAME", "BTC_LSTM_Production")
-logger.log(msg="환경변수 설정 성공")
+
+FEATURE_COLUMNS = [
+    'btc_open', 'btc_high', 'btc_low', 'btc_close', 'btc_volume',
+    'ndx_open', 'ndx_high', 'ndx_low', 'ndx_close',
+    'vix_open', 'vix_high', 'vix_low', 'vix_close',
+    'gold_open', 'gold_high', 'gold_low', 'gold_close', 'gold_volume'
+]
+
+logger.info(msg="환경변수 설정 성공")
 
 def get_db_connection():
     """
@@ -34,7 +42,7 @@ def get_db_connection():
         raise ValueError("환경 변수가 올바르게 설정되지 않았습니다.")
 
     try:
-        logger.log(msg="데이터베이스 연결 시도")
+        logger.info(msg="데이터베이스 연결 시도")
         connection = pymysql.connect(
             host=DB_HOST,
             port=DB_PORT,
@@ -43,7 +51,7 @@ def get_db_connection():
             db=DB_NAME,
             charset='utf8'
         )
-        logger.log(msg="데이터베이스 연결 성공")
+        logger.info(msg="데이터베이스 연결 성공")
     except Exception as e:
         raise ConnectionError(f"데이터베이스 연결에 실패했습니다: {e}")
 
@@ -67,7 +75,7 @@ def fetch_all_btc_four_six() -> pd.DataFrame:
             WHERE datetime BETWEEN '2025-04-01' AND '2025-06-30'
             ;
             """
-        logger.log(msg="2025-04-01 ~ 2025-06-30 BTC 데이터 fetch")
+        logger.info(msg="2025-04-01 ~ 2025-06-30 BTC 데이터 fetch")
         df = pd.read_sql_query(
             query_get_ohlcv,
             conn
@@ -86,14 +94,35 @@ def get_model_aliases():
         aliases = df['alias'].tolist()
 
         if not aliases:
-            logger.log(msg="등록된 모델 별칭이 없습니다.")
+            logger.info(msg="등록된 모델 별칭이 없습니다.")
             return ["backtest_20250531"] # 샘플 데이터용 기본 별칭
         return aliases
     except Exception as e:
         logger.error(f"DB 연결 실패: {e}")
         return ["backtest_20250531"]
 
+
+def fetch_all_features(start_date: str) -> pd.DataFrame:
+    """
+    연결된 DB에서 feature 데이터를 가져오는 함수
+    """
+    with get_db_connection() as conn:
+        query_get_features = f"""
+            SELECT datetime,
+                   btc_open, btc_high, btc_low, btc_close, btc_volume,
+                   ndx_open, ndx_high, ndx_low, ndx_close,
+                   vix_open, vix_high, vix_low, vix_close,
+                   gold_open, gold_high, gold_low, gold_close, gold_volume
+            FROM {DB_NAME}.integrated_data
+            WHERE datetime BETWEEN '{start_date}' AND '2025-06-30'
+        """
+        logger.info(msg=f"{start_date} ~ 2025-06-30 FEATURE_COLUMNS fetch")
+        df = pd.read_sql_query(query_get_features, conn)
+
+    return df.sort_values(by="datetime").reset_index(drop=True)
+
+
 if __name__ == "__main__":
-    df = fetch_all_btc_four_six("2024-02-01 00:00:00")
+    df = fetch_all_btc_four_six()
     print(df.head(25))
     print(df.count())
